af44e92f12a3ac8da3d192ce9c563010d8023ac9
Cirey Francis <thecursedmerchant@gmail.com>
2018-09-19 16:46:48 -0400

    recent 2

--- src/app/components/test/test.component.ts
+++ src/app/components/test/test.component.ts
@@ -16,7 +16,6 @@ export class TestComponent implements OnInit {
   ngOnInit() {
   }
 
-  startIndex : number = 0;
   quote : Quote = {
     symbol: undefined,
     companyName: undefined,
@@ -27,7 +26,7 @@ export class TestComponent implements OnInit {
   symbols : string[] = [];    //omg b4 didnt work...but stackoverflow said we didnt initialize our
                 //array ;D   so needed to do = []; empty array
   cireyQuotes: Quote[];
-
+  
 
   getStocks(){
     this.myService.getStocks()
@@ -49,21 +48,7 @@ export class TestComponent implements OnInit {
     }));
   }
 
-<<<<<<< HEAD
-  /*getQuotes(start:number, end:number){
-=======
-  getNextQuotes(){
-    this.getQuotes(this.startIndex, this.startIndex + 10);
-    this.startIndex += 10;
-  }
-
-  backToZero(){
-    this.startIndex = 0; //reset start inDex
-    this.getNextQuotes(); //call get tenquotes again, but with new startIndex :)
-  }
-
-  getQuotes(start:number, end:number){
->>>>>>> 01221da013a60f56c84ac6a54a0cf62bb03af150
+    getQuotes(start:number, end:number){
     //call this after getSymbols has populated this.symbols[]
     for(let i= start; i < end; i++) //for every symbol url
     {
@@ -71,20 +56,15 @@ export class TestComponent implements OnInit {
       //this quote except replace "aapl"
       let url = "https://api.iextrading.com/1.0/stock/" + this.symbols[i] + "/quote?filter=symbol,open,companyName";
       this.myService.getQuotes(url)
-<<<<<<< HEAD
-      .subscribe((apiQuotes =>{ this.quotes = apiQuotes;               //"quotes" is array of quotes
-=======
-      .subscribe((apiQuote =>{                //"quotes" is array of quotes
-          console.log(i - start);
->>>>>>> 01221da013a60f56c84ac6a54a0cf62bb03af150
+      .subscribe((apiQuote =>{ this.quotes = apiQuote;               //"quotes" is array of quotes
           this.quotes[i - start] = new QuoteClass();
           this.quotes[i - start]["symbol"] = apiQuote["symbol"];
           this.quotes[i - start]["open"] = apiQuote["open"];
           this.quotes[i - start]["companyName"] = apiQuote["companyName"];
       }));
     }
-  }*/
-
+  }
+  condition: boolean = false;
   getCireyQuotes(start:number, end:number){
     for(let i = start; i < end; i++)
     {
@@ -95,5 +75,6 @@ export class TestComponent implements OnInit {
         this.quotes[i - start].symbol = currentQuote["symbol"]; 
       });
     }
+    this.condition = true;
   }
 }
