a8dfe946c221d4d531a0947cc040daa3c8a8b1b9
Cindy <cpeng3030@gmail.com>
2018-09-24 12:17:56 -0700

    changed user setting form a bit

--- src/app/components/user-settings/user-settings.component.ts
+++ src/app/components/user-settings/user-settings.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { UserSettingsFormData } from '../../models/userSettingsFormData';
+import { UserSettingsServicesService } from '../../services/user-settings-services.service';
+import { User } from '../../models/user';
 
 @Component({
   selector: 'app-user-settings',
@@ -7,9 +10,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserSettingsComponent implements OnInit {
 
-  constructor() { }
+  userN:string;
+  oldPassW:string;
+  newPassW:string;
+  confirmPassW:string;
+  public userSettingsFormData: UserSettingsFormData;
+  public user:User;
 
+  //inject your service to instantiate it. make it private singleton
+  constructor( private userSettingsService:UserSettingsServicesService ) { }
   ngOnInit() {
   }
+
+  updateSettings(){
+    this.userSettingsFormData = 
+        new UserSettingsFormData(this.userN, this.oldPassW, this.newPassW, this.confirmPassW);
+    this.userSettingsService.postFormData(this.userSettingsFormData)
+      .subscribe(usr => {
+        this.user = usr;
+        console.log(this.user);
+      });
+  }
+
 
 }
