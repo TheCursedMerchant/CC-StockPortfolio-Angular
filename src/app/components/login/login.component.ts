42c4a966cedbac2df27b5612dd1ebe6663fe8ac7
Cirey Francis <thecursedmerchant@gmail.com>
2018-09-24 15:14:12 -0400

    Login wrap up

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -9,20 +9,71 @@ import { CredentialsService } from '../../services/credentials.service';
 })
 export class LoginComponent implements OnInit {
 
+  userId : number;
   userN: string;
   passW: string;
+  name: string;
+
+  showe : string;
+
   public user: User;
+
   constructor(private postService: CredentialsService) { }
 
   ngOnInit() {
     this.user = new User();
+    console.log(this.user);
   }
+
+  /*sendCredentials(){
+    this.user.userN = this.userN;
+    this.user.passW = this.passW;
+    this.postService.postCredentials(this.user).subscribe(res =>
+      {
+      this.user.userId = res.userId;
+      console.log(this.user.userId);
+      this.user.userN = res.userN;
+      console.log(this.user.userN);
+      this.user.passW = res.passW;
+      console.log(this.user.passW);
+      this.user.name = res.name;
+      console.log(this.user.name);
+    });
+    if(this.user.name === undefined || this.user.userN === undefined
+    || this.user.userN == undefined || this.user.userId == undefined){
+      //window.location.reload(true);
+      console.log("user not found");
+    }else{
+      //window.location.replace("home");
+      console.log("user found");
+    }
+    console.log(this.user);
+  }*/
 
   sendCredentials(){
     this.user.userN = this.userN;
     this.user.passW = this.passW;
-    this.postService.postCredentials(this.user).subscribe(res => console.log(res.userN));
-    console.log("1");
+    this.postService.postCredentials(this.user).then(res =>
+      {
+      this.user.userId = res.userId;
+      console.log(this.user.userId);
+      this.user.userN = res.userN;
+      console.log(this.user.userN);
+      this.user.passW = res.passW;
+      console.log(this.user.passW);
+      this.user.name = res.name;
+      console.log(this.user.name);
+      if(this.user.name === undefined || this.user.userN === undefined
+        || this.user.userN === undefined || this.user.userId === undefined){
+          window.location.reload(true);
+          console.log("user not found");
+          this.user.userId = undefined;
+          this.user.name = undefined;
+        }else{
+          window.location.replace("home");
+          console.log("user found");
+        }
+        console.log(this.user);
+    });
   }
-
 }
--- src/app/services/credentials.service.ts
+++ src/app/services/credentials.service.ts
@@ -11,15 +11,26 @@ export class CredentialsService {
   constructor(private myClient: HttpClient) {}
   url: string = "http://localhost:8080/login";
 
-postCredentials(usr: User): Observable<User> {
+/*postCredentials(usr: User): Observable<User> {
   
-  const headers = new HttpHeaders().set('content-type','application/json');
+  const headers = new HttpHeaders({'Access-Control-Allow-Origin':'*'}).set('content-type','application/json');
   var body = 
   {
     username: usr.userN,
     password: usr.passW
   };
   return this.myClient.post<User>(this.url, body, {headers} );
+}*/
+
+postCredentials(usr: User): Promise<User> {
+  
+  const headers = new HttpHeaders({'Access-Control-Allow-Origin':'*'}).set('content-type','application/json');
+  var body = 
+  {
+    username: usr.userN,
+    password: usr.passW
+  };
+  return this.myClient.post<User>(this.url, body, {headers} ).toPromise();
 }
 
 }
