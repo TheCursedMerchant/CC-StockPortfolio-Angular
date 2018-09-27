fc18ff8d54437a1def454c8b74fb0f060ea6016d
Cirey Francis <thecursedmerchant@gmail.com>
2018-09-27 04:43:56 -0400

    Got company stocks page awe yeaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- src/app/models/quoteClass.ts
+++ src/app/models/quoteClass.ts
@@ -2,6 +2,7 @@ export class QuoteClass {
     symbol ? : string;
     companyName ? : string;
     open ? : number;
+    latestPrice ? : number;
 
     // https://visualstudiomagazine.com/articles/2016/01/01/exploiting-typescript-arrays.aspx
     constructor(symbol, companyName, open){
